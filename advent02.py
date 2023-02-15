4d8d743e6b9e20d04484587d0cb71a8bf207d087
kagoston <79797220+kagoston@users.noreply.github.com>
2023-02-15 12:29:57 +0100

    Update advent02.py
    comments added

--- advent02.py
+++ advent02.py
@@ -1,15 +1,21 @@
 
 def get_final(filename, strategy = False):
+    '''
+    input: the strategy played
+
+    output: final score of playing Rock Paper Scissors
+
+    '''
     with open(filename, "r") as f:
         content = f.readlines()
 
-        # opponent in first column
+    # The first column: the opponent's choice
     # A for Rock,
     # B for Paper,
     # C for Scissors.
     firstColumn = {"A" : 1, "B": 2, "C" : 3}
 
-    # second column if no strategy
+    # second column: your choice if there is no strategy
     # X for Rock,
     # Y for Paper
     # Z for Scissors.
@@ -26,29 +32,44 @@ def get_final(filename, strategy = False):
 
 
 def get_score(yourShape, opponentShape):
-    # score for selecttion: 1 for Rock, 2 for Paper, and 3 for Scissors
-    # score for the outcome: 0 if you lost, 3 if the round was a draw, and 6 if you won
+    '''
+    input: shapes chosen in a single round
+    
+    The score for a single round is the sum of the followings:
+    score for the selected shape: 1 for Rock, 2 for Paper, and 3 for Scissors
+    score for the outcome: 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+    output: a score of a single round
+    '''
     diff = yourShape - opponentShape
+    # lost
     score = yourShape
+    # draw
     if opponentShape == yourShape:
         score += 3
+    # win
     elif diff == 1 or diff == -2:
         score += 6
     return score           
 
 
 def get_shape(opponentShape, aim):
+    '''
+    input: the opponents shape and the aimed outcome
+    
+    output: your shape to choose
+    '''
     # secret strategy:
     # X means you need to lose,
-    # Y means you need to end the round in a draw,
-    # Z means you need to win.
     if aim == "X":
         result = (opponentShape  - 1) if opponentShape  > 1 else 3
+    # Y means you need to end the round in a draw,
     elif aim == "Y":
         result =  opponentShape
+    # Z means you need to win.
     else:
         result = 1 + opponentShape % 3
-    print(opponentShape, aim, result)
+    
     return result
     
 
